662d943ead33b39ead2a35127b87d71b7ae0f28a
anasansari157 <anasansari157@gmail.com>
2020-08-28 00:00:56 +0530

    use article_params

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,14 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(params[:article])
+
+    @article.save
+    redirect_to @article
   end
+
+  private 
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 end
