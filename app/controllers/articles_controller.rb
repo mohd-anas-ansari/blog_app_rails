544538fe7d74eec901e4944b9dc9da687f24927e
anasansari157 <anasansari157@gmail.com>
2020-08-28 01:41:37 +0530

    update article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,20 +6,37 @@ class ArticlesController < ApplicationController
   end
   
   def new
-  end
-
-  def create
-    @article = Article.new(article_params)
-
-    p @article, "ARTICLE"
-    @article.save
-    redirect_to @article
+    @article = Article.new()
   end
 
   def show
     @article = Article.find(params[:id])
   end
 
+  def edit 
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save  
+      redirect_to @article
+    else 
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else 
+      render 'edit'
+    end
+  end
+  
   private 
     def article_params
       params.require(:article).permit(:title, :text)
